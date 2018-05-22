611dc3266023d010c07ead0595a6a1eb3d03a8c1
Ubuntu <ubuntu@ip-172-31-81-137.ec2.internal>
2018-05-22 04:48:11 +0000

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'getjoeapi',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-34-227-173-217.compute-1.amazonaws.com',
+      ref: 'origin/master',
+      ssh_options: 'StrictHostKeyChecking=no',
+      repo: 'git@github.com:mountainstar/getjoeapi.git',
+      path: '/home/ubuntu/getjoeapi',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
